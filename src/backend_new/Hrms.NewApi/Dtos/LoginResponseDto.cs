a9d51fca6d0aaae1ae95511724ea9bf01daab218
pritih8 <prith8@gmail.com>
2026-05-24 00:28:50 +0530

    Merge branch 'feature/hrms-ui' into hr_Sakshi

--- src/backend_new/Hrms.NewApi/Dtos/LoginResponseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Hrms.NewApi.Dtos;
-
-public class LoginResponseDto
-{
-    public List<ModuleGroupDto> Groups { get; set; } = new();
-
-    public List<ActivityDto> Activities { get; set; } = new();
-}
